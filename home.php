bbf2e5deeeaeb36f4300e31db448e4d94c0d3337
Cristian Echeverri <cristian@dalerojo.net>
2015-08-08 17:21:03 -0500

    Independencias de archivos. Header, Home, footer.

--- home.php
+++ home.php
@@ -1,2 +1,82 @@
 
 <?php include(TEMPLATEPATH."/header.php");?>
+
+<div class="slideruno">
+	<div class="slidcontenuno">
+		<h1>Crear tu propia tienda virtual<br> Nunca fue tan fácil!</h1><br>
+		<p>Somos una plataforma segura e intuitiva que te ayuda a vender en internet.</p>
+		<br><br>
+<div class="opcionplaneslid">
+	<a class="botonplaneslid" href=""><span class="fa fa-shopping-cart"></span> <b>Ver planes</b></a>
+</div>
+	</div>
+</div>
+
+
+<!-- Inicio Tres Pasos Home !-->
+<div class="trespasos">
+	<div class="contenedortresp">
+		<div class="pasouno">
+			<img src="http://www.wootiendas.com/wp-content/themes/bharat/images/icons/user-interface.svg" alt="">
+			<br>
+			<p>Registrar su tienda sólo<br> le tomará unos minutos</p>
+		</div>
+		<div class="pasodos">
+			<img src="http://www.wootiendas.com/wp-content/themes/bharat/images/icons/shirt.svg" alt="">
+			<br>
+			<p>Nuestro centro de soporte<br> está disponible 24/7</p>
+		</div>
+		<div class="pasotres">
+			<img src="http://www.wootiendas.com/wp-content/themes/bharat/images/icons/card.svg" alt="">
+			<br>
+			<p>Múltiples y seguras formas<br> de recibir todos tus pagos</p>
+		</div>
+	</div>
+</div>
+<div class="carauna">
+	<div class="contenedoruna">
+	<div class="textocarauna">
+	<h2>Siempre disponible, estés dónde éstes</h2><br>
+	<p>Todos nuestros diseños y funciones están disponibles para el consumo y administración desde todos los dispositivos de una forma fácil y segura.</p>
+	</div>
+	<div class="imagencarauna">
+		<img src="http://wootiendas.com/wp-content/themes/bharat/responsive.png" alt="">
+	</div>
+	</div>
+</div>
+<div class="carados">
+	<div class="contenedordos">
+	<div class="imagencarados">
+		<img src="http://wootiendas.com/wp-content/themes/bharat/cdn.png" alt="">
+	</div>
+	<div class="textoscaraos">
+		<h2>Multiples formas de recibir tus pagos</h2><br>
+		<p>Puedes cobrar tus ventas con tarjeta de crédito, consignación nacional, transferencia bancaria, cheques e incluso contra entrega, manteniendo un control y seguimiento a tus ventas.</p>
+	</div>
+	</div>
+</div>
+</div>
+
+<div class="planesfull">
+	<div class="contenedorplanfull">
+		<h1>Elije uno de nuestros dos planes</h1>
+		<h4>Crea y administra tu tienda virtual abonando una cuota anual o mensual</h4>
+		<p>También puedes adiccionar otros servicios, como dominio propio, email, chat de soporte, <br>entre otras aplicaciones disponibles en <b>Wootiendas.com</b></p>
+	</div>
+</div>
+
+<div class="opcionplanes">
+	<a class="botonplanes" href=""><span class="icon-shop"></span> <b>$25 dólares al mes</b></a>
+	<a class="botonplanes" href=""><span class="icon-shop"></span> <b>$240 dólares al año</b></a>
+</div>
+
+<div class="creditos">
+	<img src="http://wootiendas.com/wp-content/themes/bharat/creditos/fb.png" alt="">
+	<img src="http://wootiendas.com/wp-content/themes/bharat/creditos/angular.png" alt="">
+	<img src="http://wootiendas.com/wp-content/themes/bharat/creditos/bootstrap.png" alt="">
+	<img src="http://wootiendas.com/wp-content/themes/bharat/creditos/paypal.png" alt="">
+	<img src="http://wootiendas.com/wp-content/themes/bharat/creditos/wordpress.png" alt="">
+	<img src="http://wootiendas.com/wp-content/themes/bharat/creditos/woo.png" alt="">
+</div>
+
+<?php include(TEMPLATEPATH."/footer.php");?>
