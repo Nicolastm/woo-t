670f5611837ee931894083fabec6e56e00ed46ab
Cristian Echeverri <cristian@dalerojo.net>
2015-08-08 17:44:37 -0500

    Lupe código

--- single.php
+++ single.php
@@ -46,31 +46,14 @@
 		  <?php _e('Sorry, no posts matched your criteria.', 'studiopress'); ?>
 		  <?php endif; ?>
 
-		  <?php include(TEMPLATEPATH."/post-relacionados.php");?>
-		  <div style="border-bottom:3px dotted #AEAEAE; margin-bottom:12px; padding:10px 0px 0px 0px; clear:both;"></div>
 
 
-			<center><div class="fb-comments" data-href="<?php the_permalink(); ?>" data-num-posts="30" data-width="630"></center>
 
 	</div>
 
 
 		<!-- Sidebar Fanaticadas -->
 	<div class="sidebarfanaticadas">
-	 <script type="text/javascript">eplAD4M("300x250_1");</script>
-	 <br>
-	 <?php include(TEMPLATEPATH."/modulo-widget-ranking.php");?>
-	 <br>
-	 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.3&appId=85801543373";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<div class="fb-page" data-href="https://www.facebook.com/fanaticadas" data-width="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/fanaticadas"><a href="https://www.facebook.com/fanaticadas">Fanaticadas.com</a></blockquote></div></div>
-	<br>
+		<p>Hola Sidebar</p>
 	</div>
 <?php include(TEMPLATEPATH."/footer.php");?>
