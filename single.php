094aa18fc4c837acf3f4b6cfe2a6148e752d3879
Cristian Echeverri <cristian@dalerojo.net>
2015-08-08 19:21:39 -0500

    Agregamos div de comentarios

--- single.php
+++ single.php
@@ -31,6 +31,16 @@
 		width: 300px;
 		float: right;
 	}
+	.comentariosblog {
+		width: 100%;
+		margin-top: 30px;
+		border-top: 2px solid #A1A1A1;
+	}
+		.comentariosblog p {
+			font-family: lato;
+			font-size: 19px;
+			color: #A1A1A1;
+		}
 </style>
 
 <!-- Contenedor Notas -->
@@ -40,11 +50,15 @@
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           <h1><?php the_title(); ?></h1>
-          <div style="border-bottom:3px dotted #AEAEAE; margin-bottom:12px; padding:10px 0px 0px 0px; clear:both;"></div>
+          <div style="border-bottom:1px solid #AEAEAE; margin-bottom:12px; padding:10px 0px 0px 0px; clear:both;"></div>
+          <p>Comentarios | Publicado hace: 30 minutos | Por: Cristian Echeverri</p>
           <p><?php the_content(__('Read more', 'studiopress'));?></p>
 		  <?php endwhile; else: ?>
 		  <?php _e('Sorry, no posts matched your criteria.', 'studiopress'); ?>
 		  <?php endif; ?>
+		  <div class="comentariosblog">
+		  	<p>Deje su comentario a continuación:</p>
+		  </div>
 	</div>
 
 
