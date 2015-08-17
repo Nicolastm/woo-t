d4737537e0dc509d2f6c3fe20205d1da0e9218bc
Cristian Echeverri <cristian@dalerojo.net>
2015-08-17 11:03:01 -0500

    Cargar contenido

---
+++ planes.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: PLANES
+*/
+?>
+
+<?php include(TEMPLATEPATH."/header.php");?>
+
+<div class="planesfull">
+	<div class="contenedorplanfull">
+		<h1>Elije uno de nuestros dos planes</h1>
+		<h4>Crea y administra tu tienda virtual abonando una cuota anual o mensual</h4>
+		<p>También puedes adiccionar otros servicios, como dominio propio, email, chat de soporte, <br>entre otras aplicaciones disponibles en <b>Wootiendas.com</b></p>
+	</div>
+</div>
+
+<div class="opcionplanes">
+	<a class="botonplanes" href=""><span class="icon-shop"></span> <b>$25 dólares al mes</b></a>
+	<a class="botonplanes" href=""><span class="icon-shop"></span> <b>$240 dólares al año</b></a>
+</div>
+
+<?php include(TEMPLATEPATH."/footer.php");?>
