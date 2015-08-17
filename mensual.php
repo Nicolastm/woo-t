a5f85a687414df2c97ec637b1ce318acf79d17e3
Cristian Echeverri <cristian@dalerojo.net>
2015-08-17 11:13:05 -0500

    Contenido en planes
    Contenidos de los planes anual y mensual

---
+++ mensual.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Template Name: PLAN MENSUAL
+*/
+?>
+<?php include(TEMPLATEPATH."/header.php");?>
+
+<div class="hedplananual">
+	<div class="contenedorhedplananual">
+		<h1>Plan mensual  - $25 dólares</h1>
+		<p>Recibirás un periodo de prueba sin costo y compromiso alguno por 8 días. <br>Pasado este tiempo te invitaremos a realizar tu pago.</p>
+	</div>
+</div>
+
+<div class="widpanelanual">
+	<div class="contenedorwidanual">
+		<div class="formularioanual">
+			<script type="text/javascript" src="http://form.jotformz.com/jsform/52127977693671"></script>
+		</div>
+		<div class="caracteristicasanual">
+			<div class="casunoanual">
+				<p><span class="icon-shop"></span> Nombre de dominio gratis</p>
+				<p><span class="icon-shopping-cart"></span> Productos ilimitados</p>
+				<p><span class="icon-mail"></span> Cuentas de correo</p>
+				<p><span class="icon-chat"></span> Soporte en 4 idiomas!</p>
+				<p><span class="icon-credit"></span> Monedas ilimitadas</p>
+				<p><span class="icon-area-graph"></span> Sistema de cupones y afiliados</p>
+				<p><span class="icon-area-graph"></span> Estadísticas detalladas</p>
+
+			</div>
+			<div class="casadosanual">
+				<p><span class="icon-palette"></span> Personalizar fàcilmente la tienda</p>
+				<p><span class="icon-facebook"></span> Instalar tienda en tu FansPage</p>
+				<p><span class="icon-chat"></span> Chat de soporte para sus clientes</p>
+				<p><span class="icon-add-user"></span> Administradores ilimitados</p>
+				<p><span class="icon-credit-card"></span> Múltiples plataformas de pagos</p>
+				<p><span class="icon-lock"></span> Transaciones 100% seguras</p>
+				<p><span class="icon-google"></span> Posicionamiento en buscadores</p>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include(TEMPLATEPATH."/footer.php");?>
