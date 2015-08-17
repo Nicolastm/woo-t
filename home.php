603c21c5e6832ad0c13afb8dd0eae3fab0ef682d
Cristian Echeverri <cristian@dalerojo.net>
2015-08-16 23:07:44 -0500

    cambio de ortografía

--- home.php
+++ home.php
@@ -61,7 +61,7 @@
 	<div class="contenedorplanfull">
 		<h1>Elije uno de nuestros dos planes</h1>
 		<h4>Crea y administra tu tienda virtual abonando una cuota anual o mensual</h4>
-		<p>También puedes adiccionar otros servicios, como dominio propio, email, chat de soporte, <br>entre otras aplicaciones disponibles en <b>Wootiendas.com</b></p>
+		<p>También puedes adicionar otros servicios, como dominio propio, email, chat de soporte, <br>entre otras aplicaciones disponibles en <b>Wootiendas.com</b></p>
 	</div>
 </div>
 
