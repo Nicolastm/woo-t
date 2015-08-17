e4bbbc140ceaab5d2b3dd22f0173bd03531b3151
Cristian Echeverri <cristian@dalerojo.net>
2015-08-17 11:32:04 -0500

    código de chat

--- footer.php
+++ footer.php
@@ -4,5 +4,10 @@
 	<p><span class="fa fa-shopping-cart"></span> Wootiendas.com | 2015</p>
 </div>
 
+<!-- Start Live Chat Code -->
+<script type="text/javascript" src="http://www.wootiendas.com/soporte/assets/js/jquery.min.js"></script>
+<script type="text/javascript">jQuery.noConflict();</script>
+<script type="text/javascript">jQuery(document).ready(function($) {$.ajaxSetup({xhrFields: {withCredentials: true},headers: {"X-Requested-With": "XMLHttpRequest"}});$.ajax({type: "GET",url: "http://www.wootiendas.com/soporte/live_chat",dataType: "html",success: function(data) {$("body").append(data);}});});</script>
+<!-- End Live Chat Code -->
 </body>
 </html>
